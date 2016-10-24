4e7d8b83ff9daf43f23bc1b982eb70fbbfdc7818
Gui Talarico <gtalarico@gmail.com>
2016-10-24 01:08:21 -0400

    Point FlaskS3 Install to my fork

--- app/config.py
+++ app/config.py
@@ -49,6 +49,18 @@ class Development(Config):
 
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     DEBUG_TB_TEMPLATE_EDITOR_ENABLED = True
+    DEBUG_TB_PANELS = (
+        'flask_debugtoolbar.panels.versions.VersionDebugPanel',
+        'flask_debugtoolbar.panels.timer.TimerDebugPanel',
+        'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
+        'flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel',
+        'flask_debugtoolbar.panels.config_vars.ConfigVarsDebugPanel',
+        'flask_debugtoolbar.panels.template.TemplateDebugPanel',
+        'flask_debugtoolbar.panels.logger.LoggingPanel',
+        'flask_debugtoolbar.panels.route_list.RouteListDebugPanel',
+        'flask_debugtoolbar.panels.profiler.ProfilerDebugPanel',
+        # 'flask_debugtoolbar.panels.sqlalchemy.SQLAlchemyDebugPanel',
+        )
 
 
 class Testing(Config):
