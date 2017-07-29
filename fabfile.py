a45d21b879e1e2f922088760589d4a2fbde21b64
Gui Talarico <gtalarico@gmail.com>
2017-07-29 15:24:27 -0400

    Added Fabfile, rewrote configs

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,65 @@
+import os
+from configparser import ConfigParser
+
 from fabric.api import local
 
-def test():
+
+def run_tests():
     local("python -m unittest tests.tests -b -v")
 
-def commit():
-    local("git add -p && git commit")
 
-def push():
-    local("git push")
+def run(env_name='dev'):
+    """
+    >>> fab run
+    >>> fab run:staging
+    >>> fab run:production
+    """
+    load_config(env_name)
+    local("python run.py")
 
-def prepare_deploy():
-    test()
-    commit()
-    push()
+
+def load_config(env_name):
+    """
+    >>> fab run
+    >>> fab run:staging
+    >>> fab run:production
+    """
+    config = ConfigParser()
+    config.read('.env')
+
+    env = dict(config['default'])
+    env.update(dict(config[env_name]))
+
+    os.environ.update(env)
+    print('Enviroment Set: [.env:{}]'.format(env_name))
+
+
+def push(remote_name):
+    local("git push {}".format(remote_name))
+
+
+def rebuild_assets():
+    os.environ.update({'FLASK_APP': 'app/__init__.py'})
+    local('flask assets clean')
+    local('flask assets build')
+
+
+def upload_assets():
+    local("python upload-s3-assets.py")
+
+
+def deploy_staging():
+    run_tests()
+    load_config('staging')
+    rebuild_assets()
+    upload_assets()
+    push('staging')
+
+
+def deploy_production():
+    run_tests()
+    load_config('production')
+    rebuild_assets()
+    upload_assets()
+    push('heroku')
+    push('origin')
--- upload-s3-assets.py
+++ upload-s3-assets.py
@@ -5,13 +5,8 @@ import logging
 logger = logging.getLogger('flask_s3')
 logger.setLevel(logging.DEBUG)
 
-# TODO: Implement API Clear, maybe CLI
-
 bucket_name = app.config['FLASKS3_BUCKET_NAME']
 print('Uploading Assets to S3 Bucket: [{}]'.format(bucket_name))
 
-# if input('Bucket is [{}]. Confirm? [y]'.format(bucket_name)) == 'y':
 flask_s3.create_all(app)
-    # print('Done.')
-# else:
-    # print('Canceled')
+print('Done.')
